afd1784c53bb3aa96f9619c0878f8a8972cb5760
imranhasan2 <imranhasan030303@gmail.com>
2025-02-03 00:33:10 +0600

    solved problem 2

--- AssignMent1/src/problem-2.ts
+++ AssignMent1/src/problem-2.ts
@@ -1,3 +1,13 @@
 {
-  // remove duplicates
+  function removeDuplicates(arr: number[]): number[] {
+    let removeDuplicate: number[] = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (!removeDuplicate.includes(arr[i])) {
+        removeDuplicate.push(arr[i]);
+      }
+    }
+    return removeDuplicate;
+  }
+
+  // console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
 }
