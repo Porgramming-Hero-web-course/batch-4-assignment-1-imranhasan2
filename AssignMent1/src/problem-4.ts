8c56d9090ee7197950b065cfd1db150d902d1db1
imranhasan2 <imranhasan030303@gmail.com>
2025-02-04 14:08:17 +0600

    fixed problem 4

--- AssignMent1/src/problem-4.ts
+++ AssignMent1/src/problem-4.ts
@@ -1,3 +1,26 @@
 {
-  
+  type Circle = {
+    shape: "circle";
+    radius: number;
+  };
+  type Rectangle = {
+    shape: "rectangle";
+    width: number;
+    height: number;
+  };
+
+  type shape = Circle | Rectangle;
+
+  function calculateShapeArea(shape: shape): number {
+    if (shape.shape == "circle") {
+      return Math.PI * shape.radius ** 2;
+    } else if (shape.shape == "rectangle") {
+      return shape.height * shape.width;
+    } else {
+      throw new Error("shape type must be circle or rectangle");
+    }
+  }
+
+  const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+  console.log(circleArea);
 }
